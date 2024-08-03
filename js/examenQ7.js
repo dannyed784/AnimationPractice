2a7630f456ce6e65a113833fd3a7fe9ad306633c
Daniel <dannyed784@gmail.com>
2024-08-03 09:36:19 -0400

    Final examen CDI

---
+++ js/examenQ7.js
@@ -0,0 +1,11 @@
+let monAnimation = anime({
+  targets: ["#titre", ".contenu"],
+  translateX: 50,
+  duration: 2000,
+  direction: "alternate",
+  easing: "easeInOutQuad",
+  loop: false,
+});
+
+
+
